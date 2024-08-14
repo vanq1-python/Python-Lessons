ea5277fc0f78ad2c94326622c688dbc0a473234f
vanq1-python <vanq1@ya.ru>
2024-08-14 11:27:02 +0300

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,9 @@
+completed_homework = 12
+a = completed_homework
+time_spent = 1.5
+b = time_spent
+course_name = 'Python'
+c = course_name
+time_for_one_task = b / a
+d = time_for_one_task
+print('Курс:' , c,', всего задач:', a, ', затрачено часов:', b, ', среднее время выполнения', d, 'часа.' )
