d06a33c024a0c7070636b2ded7a0c2ddd1514995
vanq1-python <vanq1@ya.ru>
2024-08-15 09:38:09 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,5 @@
+immutable_var = 'cow', 12, 10.2, False
+print('Immutable tuple:', immutable_var)
+immutable_var = ['cow', 12, 10.2, False]
+immutable_var.append('pig')
+print('Mutable list:', immutable_var)
