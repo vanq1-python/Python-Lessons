544dc815d1dc7bc32c0b5820a83c77be2db13f50
vanq1-python <vanq1@ya.ru>
2024-08-24 23:24:33 +0300

    new task

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,2 +1,21 @@
-n = input('Введите число от 3 до 20: ')
-print(n)
+import random
+first_field = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+first_field = random.choice(first_field)
+
+if first_field > 2 and first_field < 21:
+    print('Число первого поля:', first_field)
+
+password = []
+
+list_second_field = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for i in list_second_field:
+    for j in list_second_field:
+        num = i + j
+        if first_field % num == 0 and i != j and j > i:
+            password.append(i)
+            password.append(j)
+
+
+print('Пароль:', ''.join(map(str, password)))
+
