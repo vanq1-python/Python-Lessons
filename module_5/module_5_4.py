cd9d6ac511c690d8edffe3c1742825a3411f704a
vanq1-python <vanq1@ya.ru>
2024-09-26 10:06:06 +0300

    bug fixes

--- module_5/module_5_4.py
+++ module_5/module_5_4.py
@@ -26,33 +26,42 @@ class House():
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        return self.__eq__(other) or self.__lt__(other)
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        return not self.__lt__(other)
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
 
     def __add__(self, value):
-        self.value = value
-        return House(self.name, self.number_of_floors + value)
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        return self
+
 
     def __radd__(self, value):
-        return House(self.name, value + self.number_of_floors)
+        return self.__add__(value)
 
-    def __iadd__(self, value):
-        self.number_of_floors += value
-        return House(self.name, self.number_of_floors)
+    def __iadd__(self, value: int):
+        return self.__add__(value)
 
     def __del__(self):
         return print(f'{self.name} снесён, но он останется в истории')
