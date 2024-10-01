1d79cf4fdcce7e35b82c4d3a0cf20a58fc6ef8c1
vanq1-python <vanq1@ya.ru>
2024-10-01 21:27:17 +0300

    new task

---
+++ module_6/module_6_1.py
@@ -0,0 +1,50 @@
+class Animal:
+    def __init__(self, name, alive = True, fed = False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+class Plant:
+    def __init__(self, name, edible = False):
+        self.name = name
+        self.edible = edible
+
+class Mammal(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name, edible = False)
+
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name, edible = True)
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+# Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
