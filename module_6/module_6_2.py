98de3989fe3f22c545bcf68d7fc5279561bc4148
vanq1-python <vanq1@ya.ru>
2024-10-03 20:53:08 +0300

    bug fix

--- module_6/module_6_2.py
+++ module_6/module_6_2.py
@@ -1,12 +1,11 @@
 class Vehicle:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
 
-    def __init__(self, owner, __model, __color, __engine_power):
+    def __init__(self, owner, model, color, engine_power):
         self.owner = str(owner)
-        self.__model = str(__model)
-        self.__color = str(__color)
-        self.__engine_power = int(__engine_power)
-
+        self.__model = str(model)
+        self.__color = str(color)
+        self.__engine_power = int(engine_power)
 
     def get_model(self):
         return f'Модель: {self.__model}'
@@ -18,13 +17,13 @@ class Vehicle:
         return f'Цвет: {self.__color}'
 
     def print_info(self):
-        print(f'{self.get_model()},\n{self.get_horsepower()},\n{self.get_color()},\nВладелец: {self.owner}')
+        print(f'{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\nВладелец: {self.owner}')
 
     def set_color(self, new_color):
         if new_color.lower() in self.__COLOR_VARIANTS:
             self.__color = str(new_color)
         else:
-            return print(f'Нельзя сменить цвет на {new_color}')
+            print(f'Нельзя сменить цвет на {new_color}')
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
@@ -39,6 +38,8 @@ vehicle1.print_info()
 vehicle1.set_color('Pink')
 vehicle1.set_color('BLACK')
 vehicle1.owner = 'Vasyok'
+vehicle1.model = 'Honda'
+vehicle1.engine_power = 100
 
 # Проверяем что поменялось
 vehicle1.print_info()
