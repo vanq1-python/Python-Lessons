df7333715856a05f735b0ea8b273494ceb6ec733
vanq1-python <vanq1@ya.ru>
2025-01-21 13:20:13 +0300

    2

---
+++ module_11/module_11_3.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+
+
+def introspection_info(obj):
+    obj_type = type(obj).__name__
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+    methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith("__")]
+    try:
+        obj_module = obj.__module__
+    except AttributeError:
+        obj_module = "Отсутствует модуль"
+    info = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': methods,
+        'module': obj_module
+    }
+    return info
+
+
+number_info = introspection_info({'dog': 'home', 'bear': 'wild'})
+pprint(number_info)
+
+
+
+
+
